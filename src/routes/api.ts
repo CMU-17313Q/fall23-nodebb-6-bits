b8c3d2809733fa15142008527f1c5292f0e21c0c
Ali Q <akq@andrew.cmu.edu>
2023-09-17 21:15:24 +0300

    add code for checkGroupMembership function

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,5 +4,21 @@ export async function fetchPostsByGroup(groupname: string): Promise<any> {
 }
 
 export function checkGroupMembership(req: any, res: any, next: any): void {
-    // Check wether user is in a group or not
+    // Check wether user is in a certain group or not
+    const { groupname } = req.params;
+    const user = req.user;
+
+    // Ensure user.groups is populated and is an array
+    if (!Array.isArray(user.groups)) {
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    // Check if the user is a member of the specified group
+    const isMember = user.groups.includes(groupname);
+
+    if (!isMember) {
+        return res.status(403).json({ error: 'Access denied' });
+    }
+
+    next();
 }

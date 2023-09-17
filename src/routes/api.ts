645357d5a78fe39db1ec7ac209b3d08199d68ab4
Ali Q <akq@andrew.cmu.edu>
2023-09-17 21:14:23 +0300

    add placeholders for new functions

---
+++ src/routes/api.ts
@@ -0,0 +1,8 @@
+export async function fetchPostsByGroup(groupname: string): Promise<any> {
+    // Implement the database query logic to retrieve posts associated with the specified group
+    // Return the fetched posts
+}
+
+export function checkGroupMembership(req: any, res: any, next: any): void {
+    // Check wether user is in a group or not
+}

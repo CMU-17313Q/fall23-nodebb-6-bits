cc2af04a664f0c99111d20c85d04cf7c5d7fddd7
Shahad <sastaneh@andrew.cmu.edu>
2023-09-17 23:16:18 +0300

    fixing the Database create.js error

--- src/topics/create.js
+++ src/topics/create.js
@@ -33,7 +33,6 @@ module.exports = function (Topics) {
             lastposttime: 0,
             postcount: 0,
             viewcount: 0,
-            isAnswered: false,
         };
 
         if (Array.isArray(data.tags) && data.tags.length) {

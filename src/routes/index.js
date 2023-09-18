085a3513fe5c4eed0c4892d58751e57c8af27cb4
Shahad <sastaneh@andrew.cmu.edu>
2023-09-18 23:38:39 +0300

    Database is linked to the button functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -58,7 +58,7 @@ _mounts.globalMod = (app, middleware, controllers) => {
 _mounts.topic = (app, name, middleware, controllers) => {
     setupPageRoute(app, `/${name}/:topic_id/:slug/:post_index?`, [], controllers.topics.get);
     setupPageRoute(app, `/${name}/:topic_id/:slug?`, [], controllers.topics.get);
-    // Create a route for updating the answer status
+    // Create a route for updating the answer status    
      //app.post('/update-answer-status', middleware, app.updateAnswerStatus);
 };
 
